108eeaa74fd13653db8328d591c6bdc99c5c2b1a
Attar74 <m.elattar.dev@gmail.com>
2025-06-21 02:31:51 +0300

    remove unused code

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,7 +8,7 @@ const projects = [
       'A voice assistant that can be used to create and manage voice prompts',
     technologies: ['Vue.js', 'Bootstrap', 'Vite'],
     imageUrl: '/images/s2s.png',
-    githubUrl: null, // Private repository
+    githubUrl: null,
     featured: true,
     link: 'https://agent.velents.ai/velents/s2s/',
     companyLink: 'https://www.linkedin.com/company/velents',
@@ -20,7 +20,7 @@ const projects = [
     description: 'A platform for applying to jobs',
     technologies: ['Vue.js', 'Nuxt', 'Tailwind CSS', 'webpack'],
     imageUrl: '/images/Faltah.png',
-    githubUrl: null, // Private repository
+    githubUrl: null,
     featured: true,
     link: 'https://faltah.ai/apply',
     companyLink: 'https://www.linkedin.com/company/faltah',
@@ -32,7 +32,7 @@ const projects = [
     description: 'AI-powered talent acquisition platform',
     technologies: ['Vue.js', 'Nuxt', 'Tailwind CSS', 'webpack'],
     imageUrl: '/images/velentsAI.png',
-    githubUrl: null, // Private repository
+    githubUrl: null,
     featured: true,
     link: 'https://velents.ai/',
     companyLink: 'https://www.linkedin.com/company/velents',
@@ -44,7 +44,7 @@ const projects = [
     description: 'A CRM for managing customer relationships and sales',
     technologies: ['Vue.js', 'Nuxt', 'Tailwind CSS', 'webpack'],
     imageUrl: '/images/ATS.png',
-    githubUrl: null, // Private repository
+    githubUrl: null,
     featured: true,
     link: 'https://crm.velents.com/',
     companyLink: 'https://www.linkedin.com/company/velents',
@@ -57,7 +57,7 @@ const projects = [
       'Search, sell, and rent. Start now — there are plenty of great options waiting for you.',
     technologies: ['Vue.js', 'Tailwind CSS', 'Vuex', 'webpack'],
     imageUrl: '/images/Hectar.png',
-    githubUrl: null, // Private repository
+    githubUrl: null,
     featured: true,
     link: 'https://hectar.io/',
     companyLink: 'https://www.linkedin.com/company/hectarapp',
@@ -70,7 +70,7 @@ const projects = [
       'HectarPlus.io is a comprehensive digital platform designed to streamline real estate operations, particularly in the Saudi Arabian market.',
     technologies: ['Vue.js', 'Tailwind CSS', 'Vuex', 'webpack'],
     imageUrl: '/images/HPlus.png',
-    githubUrl: null, // Private repository
+    githubUrl: null,
     featured: true,
     link: 'https://hectarplus.io/',
     companyLink: 'https://www.linkedin.com/company/hectarapp',
